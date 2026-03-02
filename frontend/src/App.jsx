d6345135a3bdbd34b7291a18acc33e378d2b5ecb
JAT.SANDEEP <jatsandeep275@gmail.com>
2026-03-02 23:12:56 +0530

    added the libs and shadcn for ui components and tested it a bit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-blue-500">
-        FINORA Tailwind Working
-      </h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <Button>FINORA Ready</Button>
     </div>
   )
 }
